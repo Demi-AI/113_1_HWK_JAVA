1695da5b9d76d05d1c527c9900607def66b0dd64
王羽蜨 <demi921227@gmail.com>
2024-11-15 02:12:44 +0800

    add 113_1_HWK_JAVA

---
+++ demo/src/main/java/com/example/Calculator.java
@@ -0,0 +1,40 @@
+package com.example;
+
+public class Calculator {
+    public static void main(String[] args) {
+      if (args.length != 3) {
+        System.out.println("Usage: java Calculator operand1 operator operand2");
+        System.exit(0);
+      }
+  
+      if (!isNumeric(args[0]) || !isNumeric(args[2])) {
+        System.out.println("Error: Non-numeric operand detected.");
+        System.exit(1);
+      }
+  
+      try {
+        int operand1 = Integer.parseInt(args[0]);
+        int operand2 = Integer.parseInt(args[2]);
+        int result = 0;
+  
+        switch (args[1].charAt(0)) { 
+          case '+': result = operand1 + operand2; break;
+          case '-': result = operand1 - operand2; break;
+          case '*': result = operand1 * operand2; break;
+          case '/': result = operand1 / operand2; break;
+        }
+  
+        System.out.println(args[0] + ' ' + args[1] + ' ' + args[2] + " = " + result);
+      } catch (NumberFormatException ex) {
+        System.out.println("Error: Non-numeric operand detected.");
+      }
+    }
+  
+    private static boolean isNumeric(String str) {
+      for (char c : str.toCharArray()) {
+        if (!Character.isDigit(c) && c != '-') return false;
+      }
+      return true;
+    }
+  }
+  
---
+++ demo/src/main/java/com/example/HWK12_2_MonthDays.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class HWK12_2_MonthDays {
+    public static void main(String[] args) {
+        String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        int[] dom = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("請輸入1-12其中一個數字: ");
+        
+        try {
+            int monthIndex = scanner.nextInt() - 1;
+            System.out.println(months[monthIndex] + " 有 " + dom[monthIndex] + " 天");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("輸入的數字不在範圍內");
+        }
+    }
+}
